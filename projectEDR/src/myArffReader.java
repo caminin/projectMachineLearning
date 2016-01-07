eb6a90f9103a41de6d9ee21b858a41558a2454c7
Caminin <charlot.rodolphe@gmail.com>
2016-01-07 11:46:01 +0100

    add weka

---
+++ projectEDR/src/myArffReader.java
@@ -0,0 +1,25 @@
+
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+ 
+public class myArffReader {
+	 myArffReader(){
+		 DataSource source;
+		 Instances data;
+		try {
+			source = new DataSource("../data.arff");
+			data = source.getDataSet();
+			if (data.classIndex() == -1)
+				   data.setClassIndex(data.numAttributes() - 1);
+			System.out.println(data.toString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	 }
+	 
+	 
+}
